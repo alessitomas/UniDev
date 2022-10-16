bbe6a44cf3f462c56e68551fe5801900a3f4b5ea
Eduardosmb <edubarros253@gmail.com>
2022-10-16 20:33:41 -0300

    tabela usuario + começo do fomrs

--- src/app.py
+++ src/app.py
@@ -1,21 +1,23 @@
 #from flask_login import LoginManager
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 #from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from pathlib import Path
-from model.modelos import CursoModel
+from model.modelos import CursoModel, UsuarioModel
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import *
+from werkzeug.security import generate_password_hash, check_password_hash
 
-from recursos.rotas import Curso
+from recursos.rotas import Curso, UserForm, Usuario
 from model.sql_alchemy_para_db import db
 
 # Resistente a sistema operacional
 FILE = Path(__file__).resolve()
 src_folder = FILE.parents[0]
 # caminho para a base
-rel_arquivo_db = Path('model/cursos.db')
+rel_arquivo_db = Path('model/Unidev.db')
 caminho_arq_db = src_folder / rel_arquivo_db
 
 
@@ -23,6 +25,10 @@ app = Flask(__name__)
 app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 admin = Admin(app, name='unidev', template_mode='bootstrap3')
 admin.add_view(ModelView(CursoModel, db.session))
+admin.add_view(ModelView(UsuarioModel, db.session))
+app.config['SECRET_KEY'] = "978FSFHASF8SUHFUAGF789SAGF9AS"
+
+
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{caminho_arq_db.resolve()}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
@@ -39,12 +45,28 @@ def create_tables():
 def hello_world():
     return f"<p>Hello, World!</p>"
 
-@app.route('/teste')
-def teste():
-    t = CursoModel(1,'teste','python')
-    t.save()
-
+@app.route('/usuario/adicionar', methods=['POST', 'GET'])
+def adicionar_usuario():
+    nome = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = UsuarioModel.query.filter_by(email=form.email.data).first()
+        if user is None:
+            senha_hashed = generate_password_hash(form.senha.data, 'sha256')
+            user = UsuarioModel(nome=form.nome.data, username=form.username.data, email=form.email.data, senha = senha_hashed)
+            db.session.add(user)
+            db.session.commit()
+            nome = form.nome.data
+            form.nome.data = ''
+            form.username.data = ''
+            form.email.data = ''
+            form.senha.data = ''
+        return render_template('src\templates\add_user.html', nome=nome, form=form)
+    return 'OI'
+     
 api.add_resource(Curso, '/curso/<int:id_curso>')
+api.add_resource(Usuario, '/usuario/<int:id_usuario>')
+
 
 
 if __name__ == '__main__':
