f97f515a70c61e74a64dc7ff1d081d6df3d049ec
juliapaiva1 <juliafpaiva@hotmail.com>
2022-10-17 21:09:55 -0300

    tabelas funcionando

--- src/app.py
+++ src/app.py
@@ -4,13 +4,12 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from pathlib import Path
-from model.modelos import CursoModel, UsuarioModel
+from model.modelos import *
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 from flask_login import *
 from werkzeug.security import generate_password_hash, check_password_hash
-
-from recursos.rotas import Curso, UserForm, Usuario
+from recursos.rotas import *
 from model.sql_alchemy_para_db import db
 
 # Resistente a sistema operacional
@@ -26,9 +25,10 @@ app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 admin = Admin(app, name='unidev', template_mode='bootstrap3')
 admin.add_view(ModelView(CursoModel, db.session))
 admin.add_view(ModelView(UsuarioModel, db.session))
+admin.add_view(ModelView(MatriculaModel, db.session))
+admin.add_view(ModelView(ExerciciosModel, db.session))
+admin.add_view(ModelView(RespostasModel, db.session))
 app.config['SECRET_KEY'] = "978FSFHASF8SUHFUAGF789SAGF9AS"
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{caminho_arq_db.resolve()}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
@@ -65,6 +65,9 @@ def adicionar_usuario():
     return 'OI'
      
 api.add_resource(Curso, '/curso/<int:id_curso>')
+api.add_resource(Matricula, '/usuario/<int:id_usuario>/<int:id_matricula>')
+api.add_resource(Exercicio, '/curso/<int:id_curso>/<int:id_exercicio>')
+api.add_resource(Resposta, '/curso/<int:id_curso>/<int:id_resposta>')
 api.add_resource(Usuario, '/usuario/<int:id_usuario>')
 
 
