a080b5ed423481501a96f4093e8a8f77e31c83c8
Alessi181 <tomasalessi@gmail.com>
2022-10-30 00:09:33 -0300

    versao_final

--- src/static/js/terminal.js
+++ src/static/js/terminal.js
@@ -8,31 +8,40 @@
 // print the variable
 
 if (localStorage.getItem('primeira_vez').toString() == "true") {
-    console.log('http://127.0.0.1:5000/terminal/' + localStorage.getItem('id_user_ativo') + "/curso/" + localStorage.getItem('id_curso_ativo'), "testeeeeeeeeee");
-    console.log("entrou no if")
+  
 
     fetch('http://127.0.0.1:5000/terminal/'+localStorage.getItem('id_user_ativo').toString()+'/curso/'+localStorage.getItem('id_curso_ativo').toString()).then(r =>{
         return r.json()
     }) 
     .then(corpo=>{
-        localStorage.setItem('tela', corpo.tela)
-        document.getElementById("titulo").innerHTML = corpo.titulo
-        document.getElementById("enunciado").innerHTML = corpo.enunciado
-        document.getElementById("tela").innerHTML = corpo.tela
+        if(corpo.curso_finalizado){
+            window.location.href = "http://127.0.0.1:5000/paginafinal/"
+            }
+        else{
+            localStorage.setItem('tela', corpo.tela)
+            document.getElementById("titulo").innerHTML = corpo.titulo
+            document.getElementById("enunciado").innerHTML = corpo.enunciado
+            document.getElementById("tela").innerHTML = corpo.tela
+        }
     })
 
 }
 else {
-    console.log("entrou no else")
+
     fetch('http://127.0.0.1:5000/mudarpg/'+localStorage.getItem('id_user_ativo').toString()+'/curso/'+localStorage.getItem('id_curso_ativo').toString()+ '/exercicio/' +localStorage.getItem('tela').toString()).then(r =>{
         return r.json()
     }) 
     .then(corpo=>{
-        localStorage.setItem('tela', corpo.tela)
-        document.getElementById("titulo").innerHTML = corpo.titulo
-        document.getElementById("enunciado").innerHTML = corpo.enunciado
-        document.getElementById("tela").innerHTML = corpo.tela
-    })
+        if(corpo.curso_finalizado){
+            window.location.href = "http://127.0.0.1:5000/paginafinal/"
+            }
+        else{
+            localStorage.setItem('tela', corpo.tela)
+            document.getElementById("titulo").innerHTML = corpo.titulo
+            document.getElementById("enunciado").innerHTML = corpo.enunciado
+            document.getElementById("tela").innerHTML = corpo.tela 
+        }
+        })
 
 
 }
