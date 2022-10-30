ae24ad07e5acb4a526b8b1e71eab496519c4a3eb
alanmath <59699344+alanmath@users.noreply.github.com>
2022-10-29 23:14:39 -0300

    verificar pytest certo

--- src/static/js/terminal.js
+++ src/static/js/terminal.js
@@ -12,12 +12,39 @@ console.log('http://127.0.0.1:5000/terminal/' + localStorage.getItem('id_user_at
 fetch('http://127.0.0.1:5000/terminal/'+localStorage.getItem('id_user_ativo').toString()+'/curso/'+localStorage.getItem('id_curso_ativo').toString()).then(r =>{
     return r.json()
 }) 
+
+
+
 .then(corpo=>{
     localStorage.setItem('tela', corpo.tela)
     document.getElementById("titulo").innerHTML = corpo.titulo
     document.getElementById("enunciado").innerHTML = corpo.enunciado
     document.getElementById("tela").innerHTML = corpo.tela
+
 })
 
+$(document).ready(function () {
 
+    $(function () {
 
+      var settings = {
+        "url": 'http://127.0.0.1:5000/terminal/'+localStorage.getItem('id_user_ativo').toString()+'/curso/'+localStorage.getItem('id_curso_ativo').toString(),
+        "method": "GET",
+        "timeout": 0
+      };
+      
+      $.ajax(settings).done(function () {
+        var x = localStorage.getItem('tela')
+        $("head").append(`<py-env>
+        - pytest
+        - pytest-reportlog
+        - paths:
+          - ../static/main2.py
+          - ../static/solution.py
+          - ../static/test_${x}.py
+      </py-env>`);
+      $("body").append(` <py-script src="../static/main2.py" ouput="pytest-output">
+      </py-script>`)    
+    });
+    });
+});
